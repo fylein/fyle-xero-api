94de372c7c84612cb6f31f63856358ab9ee87eb1
Sravan Kumar <sravan.kumar@fyle.in>
2020-11-03 15:52:08 +0530

    fix

--- apps/fyle/views.py
+++ apps/fyle/views.py
@@ -40,8 +40,7 @@ class ExpenseGroupView(generics.ListCreateAPIView):
         elif state == 'READY':
             return ExpenseGroup.objects.filter(
                 workspace_id=self.kwargs['workspace_id'],
-                bill__id__isnull=True,
-                banktransaction__id__isnull=True,
+                bill__id__isnull=True
             ).order_by('-updated_at')
 
     def post(self, request, *args, **kwargs):
