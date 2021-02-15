0eb484aa350e5a891d75a3bbcfba8a5c514e8a85
Shwetabh Kumar <shwetabh.kumar@fyle.in>
2021-02-15 15:42:28 +0530

    Adding script to fix db

--- apps/fyle/models.py
+++ apps/fyle/models.py
@@ -236,8 +236,8 @@ def _group_expenses(expenses, group_fields, workspace_id):
     for field in group_fields:
         if field.lower() not in ALLOWED_FIELDS:
             group_fields.pop(group_fields.index(field))
-            # field = ExpenseAttribute.objects.filter(workspace_id=workspace_id,
-            #                                         attribute_type=field.upper()).first()
+            field = ExpenseAttribute.objects.filter(workspace_id=workspace_id,
+                                                    attribute_type=field.upper()).first()
             custom_fields[field.attribute_type.lower()] = KeyTextTransform(field.display_name,
                                                                            'custom_properties')
 
