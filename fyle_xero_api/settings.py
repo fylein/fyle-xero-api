b16b489758d6cc7c149f8fa43935125511b64265
ashwinthanaraj <ashwin.t@fyle.in>
2021-04-06 15:30:05 +0530

    Update DjangoQ config - retry, timeout, catch up

--- fyle_xero_api/settings.py
+++ fyle_xero_api/settings.py
@@ -113,6 +113,9 @@ SERVICE_NAME = os.environ.get('SERVICE_NAME')
 Q_CLUSTER = {
     'name': 'fyle_xero_api',
     'save_limit': 0,
+    'retry': 14400,
+    'timeout': 3600,
+    'catch_up': False,
     'workers': 1,
     'queue_limit': 50,
     'cached': False,
