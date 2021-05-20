e9b9695d8febd2b53deccd6310ff29585a3950e6
Nilesh Pant <nileshpant6969@gmail.com>
2021-05-20 12:08:49 +0530

    lint fix

--- apps/fyle/tasks.py
+++ apps/fyle/tasks.py
@@ -87,7 +87,7 @@ def async_create_expense_groups(workspace_id: int, fund_source: List[str], task_
             if expenses:
                 workspace.last_synced_at = datetime.now()
                 workspace.save()
-  
+ 
             expense_objects = Expense.create_expense_objects(expenses, workspace_id)
 
             expense_group_objects = ExpenseGroup.create_expense_groups_by_report_id_fund_source(
