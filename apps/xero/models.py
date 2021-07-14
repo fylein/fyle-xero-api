5d92209cf05f30aadcaeddbfd9447e093cf9bd53
Sravan Kumar <sravan.kumar@fyle.in>
2021-07-14 12:40:16 +0530

    fix line_item description

--- apps/xero/models.py
+++ apps/xero/models.py
@@ -103,10 +103,10 @@ def get_expense_purpose(workspace_id, lineitem, category) -> str:
         cluster_domain['cluster_domain'], lineitem.expense_id, org_id
     )
 
-    expense_purpose = ', purpose - {0}'.format(lineitem.purpose) if lineitem.purpose else ''
+    expense_purpose = 'purpose - {0}'.format(lineitem.purpose) if lineitem.purpose else ''
     spent_at = ' spent on {0} '.format(lineitem.spent_at.date()) if lineitem.spent_at else ''
     vendor = 'vendor - {0}'.format(lineitem.vendor) if lineitem.vendor else ''
-    return 'Expense by {0} against {1}, category - {2}{3}with claim number - {3}{4} - {5}'.format(
+    return 'Expense by {0} against {1}, category - {2}{3}with claim number - {4} - {5}'.format(
         lineitem.employee_email, vendor, category, spent_at, lineitem.claim_number, expense_purpose, expense_link)
 
 
