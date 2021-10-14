bea48c24c2c9dffe9fd9c59b01110a57963aa785
ashwinthanaraj <ashwin.t@fyle.in>
2021-10-14 13:38:22 +0530

    Limit merchant characters

--- apps/fyle/models.py
+++ apps/fyle/models.py
@@ -121,7 +121,7 @@ class Expense(models.Model):
                     'settlement_id': expense['report']['settlement_id'] if expense['report'] else None,
                     'reimbursable': expense['is_reimbursable'],
                     'state': expense['state'],
-                    'vendor': expense['merchant'],
+                    'vendor': expense['merchant'][:250],
                     'cost_center': expense['cost_center']['name'] if expense['cost_center'] else None,
                     'purpose': expense['purpose'],
                     'report_id': expense['report_id'],
