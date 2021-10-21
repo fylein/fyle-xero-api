ce14d84ee63e233d969c5fca72960db5ab02028e
ashwinthanaraj <ashwin.t@fyle.in>
2021-10-21 11:51:02 +0530

    Xero Cards Mapping

--- apps/fyle/views.py
+++ apps/fyle/views.py
@@ -12,6 +12,7 @@ from apps.tasks.models import TaskLog
 from apps.workspaces.models import FyleCredential, WorkspaceGeneralSettings, Workspace
 from apps.workspaces.serializers import WorkspaceSerializer
 
+from .platform_connector import PlatformConnector
 from .tasks import create_expense_groups, schedule_expense_group_creation
 from .utils import FyleConnector
 from .models import Expense, ExpenseGroup, ExpenseGroupSettings
@@ -137,7 +138,7 @@ class ExpenseFieldsView(generics.ListAPIView):
 
     def get_queryset(self):
         attributes = ExpenseAttribute.objects.filter(
-            ~Q(attribute_type='EMPLOYEE') & ~Q(attribute_type='CATEGORY'),
+            ~Q(attribute_type='EMPLOYEE') & ~Q(attribute_type='CATEGORY') & ~Q(attribute_type='CORPORATE_CARD'),
             workspace_id=self.kwargs['workspace_id']
         ).values('attribute_type', 'display_name').distinct()
 
@@ -346,9 +347,12 @@ class SyncFyleDimensionView(generics.ListCreateAPIView):
 
             if workspace.source_synced_at is None or time_interval.days > 0:
                 fyle_credentials = FyleCredential.objects.get(workspace_id=kwargs['workspace_id'])
-                fyle_connector = FyleConnector(fyle_credentials.refresh_token, kwargs['workspace_id'])
 
+                fyle_connector = FyleConnector(fyle_credentials.refresh_token, kwargs['workspace_id'])
                 fyle_connector.sync_dimensions()
+
+                platform = PlatformConnector(fyle_credentials, kwargs['workspace_id'])
+                platform.connector.corporate_cards.sync()
 
                 workspace.source_synced_at = datetime.now()
                 workspace.save(update_fields=['source_synced_at'])
@@ -377,9 +381,12 @@ class RefreshFyleDimensionView(generics.ListCreateAPIView):
         """
         try:
             fyle_credentials = FyleCredential.objects.get(workspace_id=kwargs['workspace_id'])
-            fyle_connector = FyleConnector(fyle_credentials.refresh_token, kwargs['workspace_id'])
 
+            fyle_connector = FyleConnector(fyle_credentials.refresh_token, kwargs['workspace_id'])
             fyle_connector.sync_dimensions()
+
+            platform = PlatformConnector(fyle_credentials, kwargs['workspace_id'])
+            platform.connector.corporate_cards.sync()
 
             workspace = Workspace.objects.get(id=kwargs['workspace_id'])
             workspace.source_synced_at = datetime.now()
--- apps/xero/models.py
+++ apps/xero/models.py
@@ -9,14 +9,14 @@ from apps.fyle.models import ExpenseGroup, Expense
 from apps.mappings.models import GeneralMapping
 
 from apps.fyle.utils import FyleConnector
-from apps.workspaces.models import FyleCredential, Workspace
+from apps.workspaces.models import FyleCredential, Workspace, WorkspaceGeneralSettings
 
 
 def get_tracking_category(expense_group: ExpenseGroup, lineitem: Expense):
     mapping_settings = MappingSetting.objects.filter(workspace_id=expense_group.workspace_id).all()
 
     tracking_categories = []
-    default_expense_attributes = ['CATEGORY', 'EMPLOYEE']
+    default_expense_attributes = ['CATEGORY', 'EMPLOYEE', 'CORPORATE_CARD']
     default_destination_attributes = ['ITEM']
 
     for setting in mapping_settings:
@@ -253,13 +253,27 @@ class BankTransaction(models.Model):
                 workspace_id=expense_group.workspace_id
             ).destination.destination_id
 
-        general_mappings = GeneralMapping.objects.get(workspace_id=expense_group.workspace_id)
+        bank_account_id = None
+        workspace_general_settings = WorkspaceGeneralSettings.objects.get(workspace_id=expense_group.workspace_id)
+
+        if workspace_general_settings.map_fyle_cards_xero_bank_account:
+            bank_account = Mapping.objects.filter(
+                source_type='CORPORATE_CARD',
+                destination_type='BANK_ACCOUNT',
+                source__source_id=expense.corporate_card_id,
+                workspace_id=expense_group.workspace_id
+            ).first()
+            if bank_account:
+                bank_account_id = bank_account.destination.destination_id
+
+        if not bank_account_id:
+            bank_account_id = GeneralMapping.objects.get(workspace_id=expense_group.workspace_id).bank_account_id
 
         bank_transaction_object, _ = BankTransaction.objects.update_or_create(
             expense_group=expense_group,
             defaults={
                 'contact_id': contact_id,
-                'bank_account_code': general_mappings.bank_account_id,
+                'bank_account_code': bank_account_id,
                 'currency': expense.currency,
                 'reference': '{} - {}'.format(expense_group.id, expense.employee_email),
                 'transaction_date': get_transaction_date(expense_group),
