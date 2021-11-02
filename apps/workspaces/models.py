10d31e7f318c51c74159f78e81ad560ea0c95e5a
Ashwin Thanaraj <37061471+ashwin1111@users.noreply.github.com>
2021-11-02 13:59:21 +0530

    Add Cluster Domain in model (#72)

--- apps/workspaces/models.py
+++ apps/workspaces/models.py
@@ -46,6 +46,7 @@ class FyleCredential(models.Model):
     """
     id = models.AutoField(primary_key=True)
     refresh_token = models.TextField(help_text='Stores Fyle refresh token')
+    cluster_domain = models.CharField(max_length=255, help_text='Cluster domain', null=True)
     workspace = models.OneToOneField(Workspace, on_delete=models.PROTECT, help_text='Reference to Workspace model')
     created_at = models.DateTimeField(auto_now_add=True, help_text='Created at datetime')
     updated_at = models.DateTimeField(auto_now=True, help_text='Updated at datetime')
