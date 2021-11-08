e1193a1cc9b8d8206e3861c68a0b7f3492db2be5
Shwetabh Kumar <shwetabh.kumar@fyle.in>
2021-11-08 15:52:15 +0530

    Infra(Addition): Adding bash script for centralized setup

---
+++ centralized_run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Run db migrations
+python manage.py migrate
+
+# Creating the cache table
+python manage.py createcachetable --database cache_db
+
+# Running development server
+python manage.py runserver 0.0.0.0:8003
