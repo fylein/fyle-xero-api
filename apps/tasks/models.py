26a3844b4a9dc0da13f6e7cdc7e5fc8a3fe4891d
ashwinthanaraj <ashwin.t@fyle.in>
2021-11-23 13:51:49 +0530

    err

--- apps/tasks/models.py
+++ apps/tasks/models.py
@@ -21,7 +21,7 @@ class TaskLog(models.Model):
     bank_transaction = models.ForeignKey(BankTransaction, on_delete=models.PROTECT,
                                          help_text='Reference to Bank Transaction', null=True)
     status = models.CharField(max_length=255, help_text='Task Status')
-    detail = JSONField(help_text='Task response', null=True)
+    detail = JSONField(help_text='Task response', null=True, default={})
     xero_errors = JSONField(help_text='Xero Errors', null=True)
     created_at = models.DateTimeField(auto_now_add=True, help_text='Created at datetime')
     updated_at = models.DateTimeField(auto_now=True, help_text='Updated at datetime')
