5bc405282a051b8de1da17f3204a469baf87c657
Shwetabh Kumar <shwetabh.kumar@fyle.in>
2022-05-05 15:32:41 +0530

    Minor bug fix

--- apps/workspaces/utils.py
+++ apps/workspaces/utils.py
@@ -44,7 +44,7 @@ def generate_xero_refresh_token(authorization_code: str) -> str:
 
     if response.status_code == 200:
         response = json.loads(response.text)
-        decoded_jwt = jwt.decode(response['id_token'], algorithms=['RS256'])
+        decoded_jwt = jwt.decode(response['id_token'], verify=False)
         print(decoded_jwt)
         return response['refresh_token']
 
