d818bdf3bfe049a26e7ece457c668d69f9565710
harshithaputtaswamy <harshitha.p@fyle.in>
2022-08-09 17:23:16 +0530

    added tests for mappings module

---
+++ tests/test_check_token_health.py
@@ -0,0 +1,31 @@
+
+import ast
+import os
+import logging
+import pytest
+from apps.xero.utils import XeroConnector, XeroCredentials
+
+logger = logging.getLogger(__name__)
+
+
+@pytest.mark.django_db
+def test_token_health():
+    refresh_tokens = ast.literal_eval(os.environ.get('XERO_TESTS_REFRESH_TOKENS'))
+    num_token_expired = 0
+
+    for workspace_id in refresh_tokens.keys():
+        try:
+            xero_credentials = XeroCredentials.objects.get(workspace_id=workspace_id)
+            xero_connection = XeroConnector(credentials_object=xero_credentials, workspace_id=workspace_id)
+
+            logger.info('xero_connection succeded - %s', xero_connection)
+
+        except Exception as error:
+            num_token_expired += 1
+            logger.info('error for workspace id - %s', workspace_id)
+            logger.info('Error message - %s', error)
+
+    logger.info('refresh tokens - %s', ast.literal_eval(os.environ.get('XERO_TESTS_REFRESH_TOKENS')))
+
+    if num_token_expired != 0:
+        pytest.exit("Refresh token expired")
