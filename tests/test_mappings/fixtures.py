2fe0f5ec3a3b46a447a31a2fb294a5162d7e9350
harshithaputtaswamy <44926484+harshithaputtaswamy@users.noreply.github.com>
2022-08-16 21:50:06 +0530

    added tests for mappings module (#143)
    * added tests for mappings module
    
    * fixed comments

---
+++ tests/test_mappings/fixtures.py
@@ -0,0 +1,50 @@
+data = {
+    'fyle_category_payload': [
+        {
+            'id':1,
+            'name':'Advertising',
+            'code':'7',
+            'is_enabled':True,
+            'restricted_project_ids':[
+                
+            ]
+        },
+        {
+            'id':2,
+            'name':'Arizona Dept. of Revenue Payable',
+            'code':'89',
+            'is_enabled':True,
+            'restricted_project_ids':[
+                
+            ]
+        },
+        {
+            'id':3,
+            'name':'Automobile',
+            'code':'55',
+            'is_enabled':True,
+            'restricted_project_ids':[
+                
+            ]
+        }
+    ],
+    'cost_center_payload': [
+        {
+            'name': 'Accessories',
+            'is_enabled': True,
+            'description': 'Cost Center - Accessories, Id - 10'
+        },
+    ],
+    'general_mapping_payload':   {
+        'id':1,
+        'bank_account_name':'Business Bank Account',
+        'bank_account_id':'562555f2-8cde-4ce9-8203-0363922537a4',
+        'payment_account_name':'Sample',
+        'payment_account_id':'sdfghjk',
+        'default_tax_code_name':'Tax on Purchases @8.25%',
+        'default_tax_code_id':'INPUT',
+        'created_at':'2022-08-02T20:27:35.368956Z',
+        'updated_at':'2022-08-02T20:27:35.368996Z',
+        'workspace':1
+    },
+}
---
+++ tests/test_mappings/test_signals.py
@@ -0,0 +1,108 @@
+import pytest
+from datetime import datetime, timezone
+from django_q.models import Schedule
+from fyle_accounting_mappings.models import MappingSetting, Mapping, ExpenseAttribute, EmployeeMapping
+from apps.mappings.models import TenantMapping
+from apps.workspaces.models import Workspace
+
+
+def test_run_post_mapping_settings_triggers(db, mocker, test_connection):
+    mocker.patch(
+        'fyle_integrations_platform_connector.apis.ExpenseCustomFields.post',
+        return_value=[]
+    )
+    workspace_id = 1
+
+    mapping_setting = MappingSetting(
+        source_field='PROJECT',
+        destination_field='PROJECT',
+        workspace_id=workspace_id,
+        import_to_fyle=True,
+        is_custom=False
+    )
+
+    mapping_setting.save()
+
+    schedule = Schedule.objects.filter(
+        func='apps.mappings.tasks.auto_create_project_mappings',
+        args='{}'.format(workspace_id),
+    ).first()
+
+    assert schedule.func == 'apps.mappings.tasks.auto_create_project_mappings'
+    assert schedule.args == '1'
+
+    mapping_setting = MappingSetting(
+        source_field='COST_CENTER',
+        destination_field='CLASS',
+        workspace_id=workspace_id,
+        import_to_fyle=True,
+        is_custom=False
+    )
+    mapping_setting.save()
+
+    schedule = Schedule.objects.filter(
+        func='apps.mappings.tasks.auto_create_cost_center_mappings',
+        args='{}'.format(workspace_id),
+    ).first()
+
+    assert schedule.func == 'apps.mappings.tasks.auto_create_cost_center_mappings'
+    assert schedule.args == '1'
+
+    mapping_setting = MappingSetting(
+        source_field='SAMPLEs',
+        destination_field='SAMPLEs',
+        workspace_id=workspace_id,
+        import_to_fyle=True,
+        is_custom=True
+    )
+    mapping_setting.save()
+
+    schedule = Schedule.objects.filter(
+        func='apps.mappings.tasks.async_auto_create_custom_field_mappings',
+        args='{}'.format(workspace_id),
+    ).first()
+
+    assert schedule.func == 'apps.mappings.tasks.async_auto_create_custom_field_mappings'
+    assert schedule.args == '1'
+
+
+def test_run_pre_mapping_settings_triggers(db, mocker, test_connection):
+    mocker.patch(
+        'fyle_integrations_platform_connector.apis.ExpenseCustomFields.post',
+        return_value=[]
+    )
+    workspace_id = 1
+
+    custom_mappings = Mapping.objects.filter(workspace_id=workspace_id, source_type='CUSTOM_INTENTs').count()
+    assert custom_mappings == 0
+
+    mapping_setting = MappingSetting(
+        source_field='CUSTOM_INTENTs',
+        destination_field='CUSTOM_INTENTs',
+        workspace_id=workspace_id,
+        import_to_fyle=True,
+        is_custom=True
+    )
+    mapping_setting.save()
+
+    custom_mappings = Mapping.objects.last()
+    
+    custom_mappings = Mapping.objects.filter(workspace_id=workspace_id, source_type='CUSTOM_INTENTs').count()
+    assert custom_mappings == 0
+
+
+def test_run_post_tenant_mapping_trigger(db, test_connection):
+
+    workspace = Workspace.objects.create(
+        id=97,
+        name = 'Fyle for sample',
+        fyle_org_id = 'Testing',
+    )
+    workspace.save()
+
+    tenant_mapping = TenantMapping(
+        tenant_name='sample',
+        tenant_id='wertyui',
+        workspace=workspace
+    )
+    tenant_mapping.save()
