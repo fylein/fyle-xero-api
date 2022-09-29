a2fdb0917a93aee4526c4b29dec59d98bbcd6424
labhvam5 <labhvam.s@fyle.in>
2022-09-29 23:34:04 +0530

    adding test

---
+++ tests/test_workspaces/test_apis/test_advanced_settings/fixtures.py
@@ -0,0 +1,44 @@
+data = {
+    'advanced_settings': {
+        "workspace_general_settings": {
+            "change_accounting_period": True,
+            "sync_fyle_to_xero_payments": False,
+            "sync_xero_to_fyle_payments": True,
+            "auto_create_destination_entity": True
+        },
+        "general_mappings": {
+            "payment_account": {
+                "id": "2",
+                "name": "Business Savings Account"
+            }
+        },
+        "workspace_schedules": {
+            "enabled": True,
+            "interval_hours": 1
+        }
+    },
+    'advanced_settings_response': {
+        "workspace_general_settings": {
+            "change_accounting_period": True,
+            "sync_fyle_to_xero_payments": False,
+            "sync_xero_to_fyle_payments": True,
+            "auto_create_destination_entity": True
+        },
+        "general_mappings": {
+            "payment_account": {
+                "id": "2",
+                "name": "Business Savings Account"
+            }
+        },
+        "workspace_schedules": {
+            "enabled": True,
+            "interval_hours": 1
+        },
+        "workspace_id": 1
+    },
+    'advanced_settings_missing_values': {
+        "workspace_general_settings": {},
+        "general_mappings": {},
+        "workspace_schedules": {}
+    },
+}
