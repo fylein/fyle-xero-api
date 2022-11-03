13f9a5a2982006dac0fdd755def7d8bb9ddb4c11
labhvam5 <labhvam.s@fyle.in>
2022-11-03 17:55:44 +0530

    lint fix

--- apps/workspaces/signals.py
+++ apps/workspaces/signals.py
@@ -9,7 +9,6 @@ def post_delete_xero_connection(workspace_id):
     """
     workspace = Workspace.objects.get(id=workspace_id)
     if workspace.onboarding_state in ('CONNECTION', 'EXPORT_SETTINGS'):
-        print("This block is reached ")
         tenant_mapping = TenantMapping.objects.filter(workspace_id=workspace_id).first()
         tenant_mapping.tenant_name = None
         tenant_mapping.tenant_id = None
