313881b020ac9dd12229cdc30e5c94d8ce088cc9
anishfyle <anishkumar.s@fyle.in>
2023-03-17 17:03:28 +0530

    check

--- apps/workspaces/tasks.py
+++ apps/workspaces/tasks.py
@@ -151,14 +151,14 @@ def run_email_notification(workspace_id):
     )
 
     task_logs_count = get_failed_task_logs_count(workspace_id)
-
     workspace = Workspace.objects.get(id=workspace_id)
     tenant_detail = TenantMapping.get_tenant_details(workspace_id)
-    if task_logs_count and (ws_schedule.error_count is None or task_logs_count > ws_schedule.error_count):
+    # task_logs_count and (ws_schedule.error_count is None or task_logs_count > ws_schedule.error_count):
+    if True:
         errors = get_errors(workspace_id)
         for admin_email in ws_schedule.emails_selected:
             admin_name = get_admin_name(workspace_id, admin_email, ws_schedule)
-            error_types = {error.type.lower().title().replace('_', ' ') for error in errors}
+            error_types = {error.type.title().replace('_', ' ') for error in errors}
             context = {
                 'name': admin_name,
                 'errors_count': task_logs_count,
