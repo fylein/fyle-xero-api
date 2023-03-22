9b7db3845bf5737bf70ed00314cd562867f4e6e3
anishfyle <anishkumar.s@fyle.in>
2023-03-22 12:05:49 +0530

    pr comment

--- apps/workspaces/templatetags/custom_filters.py
+++ apps/workspaces/templatetags/custom_filters.py
@@ -3,5 +3,5 @@ from django import template
 register = template.Library()
 
 @register.filter
-def snake_to_space(value):
+def snake_case_to_space_case(value):
     return ' '.join(word.capitalize() for word in value.split('_'))
