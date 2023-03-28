13b29dcb617a45818685fd26fe02ec23783106f6
anishfyle <anishkumar.s@fyle.in>
2023-03-28 12:11:48 +0530

    encryption_key

--- fyle_xero_api/settings.py
+++ fyle_xero_api/settings.py
@@ -277,6 +277,9 @@ XERO_CLIENT_ID = os.environ.get('XERO_CLIENT_ID')
 XERO_CLIENT_SECRET = os.environ.get('XERO_CLIENT_SECRET')
 XERO_REDIRECT_URI = os.environ.get('XERO_REDIRECT_URI')
 XERO_TOKEN_URI = os.environ.get('XERO_TOKEN_URI')
+ENCRYPTION_KEY = os.environ.get('ENCRYPTION_KEY')
+E2E_TESTS_CLIENT_SECRET = os.environ.get('E2E_TESTS_CLIENT_SECRET')
+E2E_TESTS_REALM_ID = os.environ.get('E2E_TESTS_REALM_ID')
 
 
 # Sendgrid Settings
