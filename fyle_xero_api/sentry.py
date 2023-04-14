c74e812e61a52c11e365188a70a0a0ae1fdd6ced
Rushikesh Tayade <ruuushhh2001@gmail.com>
2023-04-14 20:44:43 +0530

    Sentry integration

---
+++ fyle_xero_api/sentry.py
@@ -0,0 +1,36 @@
+import os
+
+import sentry_sdk
+
+from sentry_sdk.integrations.django import DjangoIntegration
+
+class Sentry:
+
+    @staticmethod
+    def init():
+        sentry_sdk.init(
+            dsn=os.environ.get('SENTRY_DSN'),
+            send_default_pii=True,
+            integrations=[DjangoIntegration()],
+            environment=os.environ.get('SENTRY_ENV'),
+            traces_sampler=Sentry.traces_sampler,
+            attach_stacktrace=True,
+            request_bodies='small',
+            in_app_include=['apps.users',
+            'apps.workspaces',
+            'apps.mappings',
+            'apps.fyle',
+            'apps.xero',
+            'apps.tasks',
+            'fyle_rest_auth',
+            'fyle_accounting_mappings'],
+        )
+
+    @staticmethod
+    def traces_sampler(sampling_context):
+        # avoiding ready APIs in performance tracing
+        if sampling_context.get('wsgi_environ') is not None:
+            if sampling_context['wsgi_environ']['PATH_INFO'] in ['/ready']:
+                return 0
+
+        return 0.5
