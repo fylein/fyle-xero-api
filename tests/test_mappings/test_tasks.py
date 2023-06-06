f4aa4eb813e4c431b36006a7f78a7ab41c7c1893
Ashutosh singh <55102089+Ashutosh619-sudo@users.noreply.github.com>
2023-06-06 14:19:18 +0530

    Update coa in Fyle if COA in Xero is disbled (#237)
    * disable expense attribute and updated the category payload creation
    
    * added filter to get only active expense attributes
    
    * added code to cover the new added lines
    
    * made changes to test
    
    * code covered for new features
    
    * bumping up acounting mapping version
    
    * added sql query to update active state
    
    * remove print statements
    
    ---------
    
    Co-authored-by: Ashutosh619-sudo <ashutoshsinghkrkt@gmail.com>

--- tests/test_mappings/test_tasks.py
+++ tests/test_mappings/test_tasks.py
@@ -155,8 +155,19 @@ def test_upload_categories_to_fyle(db, mocker):
         'apps.xero.utils.XeroConnector.sync_accounts',
         return_value=[]
     )
-    
+
     workspace_id = 1
+
+    da_instance = DestinationAttribute.objects.filter(workspace_id=workspace_id, attribute_type="ACCOUNT").first()
+    da_instance.active = False
+    da_instance.save()
+
+    ea_instance = ExpenseAttribute.objects.filter(attribute_type="CATEGORY").first()
+    ea_instance.active = True
+
+    mapping_instance = Mapping.objects.create(destination_type="ACCOUNT",destination=da_instance,workspace_id=workspace_id,source=ea_instance)
+
+    ea_instance.save()
 
     xero_attributes = upload_categories_to_fyle(workspace_id=workspace_id)
     assert len(xero_attributes) == 29
