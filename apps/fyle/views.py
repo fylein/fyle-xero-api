9b4c2faf3b1df6463484eaf762984902411bd558
Ashutosh singh <55102089+Ashutosh619-sudo@users.noreply.github.com>
2023-08-01 17:19:10 +0530

    remove dead api fyle app (#243)
    * remove dead api fyle app
    
    * remove deleted imports
    
    * removed some more dead api's and fucntions
    
    * remove dead api's for xero app
    
    * Revert "remove dead api's for xero app"
    
    This reverts commit 9b5afbbfb55ef0cecdbff7c55863ddca0c1569a6.
    
    ---------
    
    Co-authored-by: Ashutosh619-sudo <ashutoshsinghkrkt@gmail.com>

--- apps/fyle/views.py
+++ apps/fyle/views.py
@@ -15,7 +15,7 @@ from apps.workspaces.models import FyleCredential, WorkspaceGeneralSettings, Wor
 from apps.workspaces.serializers import WorkspaceSerializer
 
 from fyle_integrations_platform_connector import PlatformConnector
-from .tasks import create_expense_groups, schedule_expense_group_creation, get_task_log_and_fund_source, async_create_expense_groups
+from .tasks import create_expense_groups, get_task_log_and_fund_source, async_create_expense_groups
 from .models import Expense, ExpenseGroup, ExpenseGroupSettings
 from .serializers import ExpenseGroupSerializer, ExpenseSerializer, ExpenseFieldSerializer, \
     ExpenseGroupSettingsSerializer
@@ -92,19 +92,6 @@ class ExpenseGroupSettingsView(generics.ListCreateAPIView):
         )
 
 
-class ExpenseCustomFieldsView(generics.ListCreateAPIView):
-    """
-    Project view
-    """
-    serializer_class = ExpenseAttributeSerializer
-    pagination_class = None
-
-    def get_queryset(self):
-        attribute_type = self.request.query_params.get('attribute_type')
-
-        return ExpenseAttribute.objects.filter(
-            attribute_type=attribute_type, workspace_id=self.kwargs['workspace_id']).order_by('value')
-
 class ExpenseFieldsView(generics.ListAPIView):
     pagination_class = None
     serializer_class = ExpenseFieldSerializer
@@ -130,22 +117,6 @@ class ExpenseFieldsView(generics.ListAPIView):
         )
 
 
-class ExpenseGroupScheduleView(generics.CreateAPIView):
-    """
-    Create expense group schedule
-    """
-
-    def post(self, request, *args, **kwargs):
-        """
-        Post expense schedule
-        """
-        schedule_expense_group_creation(kwargs['workspace_id'])
-
-        return Response(
-            status=status.HTTP_200_OK
-        )
-
-
 class ExpenseGroupSyncView(generics.CreateAPIView):
     """
     Create expense groups
@@ -161,102 +132,6 @@ class ExpenseGroupSyncView(generics.CreateAPIView):
         return Response(
             status=status.HTTP_200_OK
         )
-
-
-class ExpenseGroupByIdView(generics.RetrieveAPIView):
-    """
-    Expense Group by Id view
-    """
-
-    def get(self, request, *args, **kwargs):
-        """
-        Get expenses
-        """
-        try:
-            expense_group = ExpenseGroup.objects.get(
-                workspace_id=kwargs['workspace_id'], pk=kwargs['expense_group_id']
-            )
-
-            return Response(
-                data=ExpenseGroupSerializer(expense_group).data,
-                status=status.HTTP_200_OK
-            )
-
-        except ExpenseGroup.DoesNotExist:
-            return Response(
-                data={
-                    'message': 'Expense group not found'
-                },
-                status=status.HTTP_400_BAD_REQUEST
-            )
-
-
-class ExpenseView(generics.RetrieveAPIView):
-    """
-    Expense view
-    """
-
-    def get(self, request, *args, **kwargs):
-        """
-        Get expenses
-        """
-        try:
-            expense_group = ExpenseGroup.objects.get(
-                workspace_id=kwargs['workspace_id'], pk=kwargs['expense_group_id']
-            )
-            expenses = Expense.objects.filter(
-                id__in=expense_group.expenses.values_list('id', flat=True)).order_by('-updated_at')
-            return Response(
-                data=ExpenseSerializer(expenses, many=True).data,
-                status=status.HTTP_200_OK
-            )
-
-        except ExpenseGroup.DoesNotExist:
-            return Response(
-                data={
-                    'message': 'Expense group not found'
-                },
-                status=status.HTTP_400_BAD_REQUEST
-            )
-
-
-class EmployeeView(generics.ListCreateAPIView):
-    """
-    Employee view
-    """
-
-    serializer_class = ExpenseAttributeSerializer
-    pagination_class = None
-
-    def get_queryset(self):
-        return ExpenseAttribute.objects.filter(
-            attribute_type='EMPLOYEE', workspace_id=self.kwargs['workspace_id']).order_by('value')
-
-
-class CategoryView(generics.ListCreateAPIView):
-    """
-    Category view
-    """
-
-    serializer_class = ExpenseAttributeSerializer
-    pagination_class = None
-
-    def get_queryset(self):
-        return ExpenseAttribute.objects.filter(
-            attribute_type='CATEGORY', workspace_id=self.kwargs['workspace_id']).order_by('value')
-
-
-class CostCenterView(generics.ListCreateAPIView):
-    """
-    Category view
-    """
-
-    serializer_class = ExpenseAttributeSerializer
-    pagination_class = None
-
-    def get_queryset(self):
-        return ExpenseAttribute.objects.filter(
-            attribute_type='COST_CENTER', workspace_id=self.kwargs['workspace_id']).order_by('value')
 
 
 class SyncFyleDimensionView(generics.ListCreateAPIView):
@@ -324,18 +199,6 @@ class RefreshFyleDimensionView(generics.ListCreateAPIView):
                 },
                 status=status.HTTP_400_BAD_REQUEST
             )
-
-
-class ProjectView(generics.ListCreateAPIView):
-    """
-    Project view
-    """
-    serializer_class = ExpenseAttributeSerializer
-    pagination_class = None
-
-    def get_queryset(self):
-        return ExpenseAttribute.objects.filter(
-            attribute_type='PROJECT', workspace_id=self.kwargs['workspace_id']).order_by('value')
 
 
 class ExportableExpenseGroupsView(generics.RetrieveAPIView):
