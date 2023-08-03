be9a138bffd11ef65efc2b6abae0c4f737ecfc03
Ashutosh singh <55102089+Ashutosh619-sudo@users.noreply.github.com>
2023-08-03 16:17:28 +0530

    remove dead api's mappings app (#246)
    * remove dead api fyle app
    
    * remove deleted imports
    
    * removed some more dead api's and fucntions
    
    * remove dead api's for xero app
    
    * Revert "remove dead api's for xero app"
    
    This reverts commit 9b5afbbfb55ef0cecdbff7c55863ddca0c1569a6.
    
    * remove dead api's for xero app
    
    * remove dead apis' workspace app
    
    * added django-filter in req.txt
    
    * removing test parts
    
    * pytest cov to 90
    
    * removal dead api's for mappings app
    
    ---------
    
    Co-authored-by: Ashutosh619-sudo <ashutoshsinghkrkt@gmail.com>

--- apps/mappings/views.py
+++ apps/mappings/views.py
@@ -87,47 +87,6 @@ class TenantMappingView(generics.ListCreateAPIView):
             )
 
 
-class GeneralMappingView(generics.ListCreateAPIView):
-    """
-    General mappings view
-    """
-    serializer_class = GeneralMappingSerializer
-
-    def post(self, request, *args, **kwargs):
-        """
-        Post General mapping view
-        """
-        general_mapping_payload = request.data
-
-        assert_valid(general_mapping_payload is not None, 'Request body is empty')
-        mapping_utils = MappingUtils(kwargs['workspace_id'])
-        general_mapping_object = mapping_utils.create_or_update_general_mapping(general_mapping_payload)
-
-        return Response(
-            data=self.serializer_class(general_mapping_object).data,
-            status=status.HTTP_200_OK
-        )
-
-    def get(self, request, *args, **kwargs):
-        """
-        Get general mappings
-        """
-        try:
-            general_mapping = GeneralMapping.objects.get(workspace_id=kwargs['workspace_id'])
-
-            return Response(
-                data=self.serializer_class(general_mapping).data,
-                status=status.HTTP_200_OK
-            )
-        except GeneralMapping.DoesNotExist:
-            return Response(
-                {
-                    'message': 'General mappings do not exist for the workspace'
-                },
-                status=status.HTTP_400_BAD_REQUEST
-            )
-
-
 class AutoMapEmployeeView(generics.CreateAPIView):
     """
     Auto Map Employees view
