872d7d019fd34cb5974a3da3b723e2b766ae2b53
Ashutosh619-sudo <ashutoshsinghkrkt@gmail.com>
2023-08-03 15:31:40 +0530

    removal dead api's for mappings app

--- apps/mappings/urls.py
+++ apps/mappings/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path, include
 
-from .views import TenantMappingView, GeneralMappingView, AutoMapEmployeeView
+from .views import TenantMappingView, AutoMapEmployeeView
 
 urlpatterns = [
     path('tenant/', TenantMappingView.as_view()),
-    path('general/', GeneralMappingView.as_view()),
     path('', include('fyle_accounting_mappings.urls')),
     path('auto_map_employees/trigger/', AutoMapEmployeeView.as_view())
 ]
