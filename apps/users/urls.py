6c75afc56823b28c6ea3bd40ed9f2d01a40ee2af
Ashutosh singh <55102089+Ashutosh619-sudo@users.noreply.github.com>
2023-08-05 23:13:41 +0530

    Remove dead api's User app (#248)
    * remove dead api fyle app
    
    * remove deleted imports
    
    * removed some more dead api's and fucntions
    
    * remove dead api's for xero app
    
    * Revert "remove dead api's for xero app"
    
    This reverts commit 9b5afbbfb55ef0cecdbff7c55863ddca0c1569a6.
    
    * remove dead api's for xero app
    
    * remove dead apis' workspace app
    
    * added django-filter in req.txt
    
    * removing test parts
    
    * pytest cov to 90
    
    * removal dead api's for mappings app
    
    * remove dead api's tasks app
    
    * line at the end of the file
    
    * remove dead api users app
    
    ---------
    
    Co-authored-by: Ashutosh619-sudo <ashutoshsinghkrkt@gmail.com>

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,15 +1,15 @@
 from django.urls import path
 
-from .views import UserProfileView, FyleOrgsView
+from .views import FyleOrgsView
 
 from apps.workspaces.apis.clone_settings.views \
     import CloneSettingsExistsView
 
 urlpatterns = [
-    path('profile/', UserProfileView.as_view()),
     path('orgs/', FyleOrgsView.as_view()),
     path(
         'clone_settings/exists/',
         CloneSettingsExistsView.as_view()
     ),
 ]
+    
--- apps/users/views.py
+++ apps/users/views.py
@@ -11,31 +11,6 @@ from apps.fyle.helpers import get_fyle_orgs
 
 from apps.users.helpers import get_cluster_domain_and_refresh_token
 
-class UserProfileView(generics.RetrieveAPIView):
-
-    permission_classes = [IsAuthenticated]
-
-    def get(self, request, *args, **kwargs):
-        """
-        Get User Details
-        """
-        refresh_token = AuthToken.objects.get(user__user_id=request.user).refresh_token
-
-        cluster_domain, _ = get_cluster_domain_and_refresh_token(request.user)
-
-        fyle_credentials = FyleCredential(
-            cluster_domain=cluster_domain,
-            refresh_token=refresh_token
-        )
-
-        platform = PlatformConnector(fyle_credentials)
-        employee_profile = platform.connection.v1beta.spender.my_profile.get()
-
-        return Response(
-            data=employee_profile,
-            status=status.HTTP_200_OK
-        )
-
 
 class FyleOrgsView(generics.ListCreateAPIView):
     """
@@ -56,3 +31,4 @@ class FyleOrgsView(generics.ListCreateAPIView):
             data=fyle_orgs,
             status=status.HTTP_200_OK
         )
+    
