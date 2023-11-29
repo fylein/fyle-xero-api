1f9e70e4232e58a178f1b3fed9c73c536fdd5207
Shwetabh Kumar <shwetabh.kumar@fyle.in>
2023-11-29 17:17:52 +0530

    Fix: Replacing all strings with enums

---
+++ apps/fyle/enums.py
@@ -0,0 +1,47 @@
+"""
+This file contains all the enums used in the Fyle app
+"""
+
+class FundSourceEnum:
+    """
+    Enum for Fund Source
+    """
+    PERSONAL = 'PERSONAL'
+    CCC = 'CCC'
+
+
+class FyleAttributeEnum:
+    """
+    Enum for Fyle Attributes
+    """
+    CATEGORY = 'CATEGORY'
+    CATEGORY_DISPLAY = 'Category'
+
+    PROJECT = 'PROJECT'
+    PROJECT_DISPLAY = 'Project'
+
+    COST_CENTER = 'COST_CENTER'
+    COST_CENTER_DISPLAY = 'Cost Center'
+
+    CORPORATE_CARD = 'CORPORATE_CARD'
+    CORPORATE_CARD_DISPLAY = 'Corporate Card'
+
+    TAX_GROUP = 'TAX_GROUP'
+    TAX_GROUP_DISPLAY = 'Tax Group'
+
+
+class ExpenseStateEnum:
+    """
+    Enum for Expense State
+    """
+    APPROVED = 'APPROVED'
+    PAYMENT_PROCESSING = 'PAYMENT_PROCESSING'
+    PAID = 'PAID'
+
+
+class PlatformExpensesEnum:
+    """
+    Enum for Platform Expenses
+    """
+    PERSONAL_CASH_ACCOUNT = 'PERSONAL_CASH_ACCOUNT'
+    PERSONAL_CORPORATE_CREDIT_CARD_ACCOUNT = 'PERSONAL_CORPORATE_CREDIT_CARD_ACCOUNT'
