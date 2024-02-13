0d0f14dc2701a51274dec6c0553a277132859e21
ruuushhh <rushikeshtayade2001@gmail.com>
2024-02-13 19:47:35 +0530

    Add tests direct export

---
+++ tests/test_fyle/test_queue.py
@@ -0,0 +1,44 @@
+from apps.fyle.models import Expense
+from apps.workspaces.models import FyleCredential, XeroCredentials
+from apps.xero.queue import __create_chain_and_run
+from apps.fyle.queue import async_post_accounting_export_summary, async_import_and_export_expenses
+
+from apps.xero.utils import XeroConnector
+
+
+# This test is just for cov :D
+def test_async_post_accounting_export_summary(db):
+    async_post_accounting_export_summary(1, 1)
+    assert True
+
+# This test is just for cov :D
+def test_create_chain_and_run(db):
+    workspace_id = 1
+    fyle_credentials = FyleCredential.objects.get(workspace_id=workspace_id)
+    in_progress_expenses = Expense.objects.filter(org_id='or79Cob97KSh')
+    xero_credentials = XeroCredentials.get_active_xero_credentials(workspace_id)
+    xero_connection = XeroConnector(xero_credentials, workspace_id)
+    chain_tasks = [
+        {
+            'target': 'apps.sage_intacct.tasks.create_bill',
+            'expense_group_id': 1,
+            'task_log_id': 1,
+            'last_export': True
+        }
+    ]
+
+    __create_chain_and_run(fyle_credentials, xero_connection, in_progress_expenses, workspace_id, chain_tasks, 'PERSONAL')
+    assert True
+
+
+# This test is just for cov :D
+def test_async_import_and_export_expenses(db):
+    body = {
+        'action': 'ACCOUNTING_EXPORT_INITIATED',
+        'data': {
+            'id': 'rp1s1L3QtMpF',
+            'org_id': 'or79Cob97KSh'
+        }
+    }
+
+    async_import_and_export_expenses(body)
--- tests/test_fyle/test_task.py
+++ tests/test_fyle/test_task.py
@@ -2,11 +2,13 @@ from unittest import mock
 
 from fyle.platform.exceptions import InvalidTokenError as FyleInvalidTokenError
 
-from apps.fyle.models import ExpenseGroupSettings
-from apps.fyle.tasks import create_expense_groups
+from apps.fyle.models import ExpenseGroupSettings, Expense, ExpenseGroup
+from django.db.models import Q
 from apps.tasks.models import TaskLog
-from apps.workspaces.models import FyleCredential
+from apps.workspaces.models import FyleCredential, Workspace
 from tests.test_fyle.fixtures import data
+from apps.fyle.tasks import create_expense_groups, post_accounting_export_summary
+from apps.fyle.actions import update_failed_expenses
 
 
 def test_create_expense_groups(mocker, db):
@@ -60,3 +62,27 @@ def test_create_expense_groups(mocker, db):
 
     task_log = TaskLog.objects.get(id=task_log.id)
     assert task_log.status == "FATAL"
+
+
+def test_post_accounting_export_summary(db, mocker):
+    expense_group = ExpenseGroup.objects.filter(workspace_id=1).first()
+    expense_id = expense_group.expenses.first().id
+    expense_group.expenses.remove(expense_id)
+
+    workspace = Workspace.objects.get(id=1)
+
+    expense = Expense.objects.filter(id=expense_id).first()
+    expense.workspace_id = 1
+    expense.save()
+
+    update_failed_expenses(Q(), [expense_id], workspace)
+
+    assert Expense.objects.filter(id=expense_id).first().accounting_export_summary['synced'] == False
+
+    mocker.patch(
+        'fyle_integrations_platform_connector.apis.Expenses.post_bulk_accounting_export_summary',
+        return_value=[]
+    )
+    post_accounting_export_summary('orPJvXuoLqvJ', 1)
+
+    assert Expense.objects.filter(id=expense_id).first().accounting_export_summary['synced'] == True
