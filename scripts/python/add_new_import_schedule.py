55d9050bef0819dab6828b0d188368e0ebaa9512
Hrishabh Tiwari <hrishabh.t@fyle.in>
2024-03-08 16:42:23 +0530

    Modified the workflow for submodule

--- scripts/python/add_new_import_schedule.py
+++ scripts/python/add_new_import_schedule.py
@@ -17,17 +17,14 @@ try:
     with transaction.atomic():
         for schedule in existing_import_schedules:
             workspace_id = schedule['args']
-
             is_new_schedule_created = Schedule.objects.filter(
                 func='apps.mappings.queue.construct_tasks_and_chain_import_fields_to_fyle',
                 args=workspace_id
             ).exists()
-
             if not is_new_schedule_created:
                 old_schedule = Schedule.objects.filter(
                     args=workspace_id
                 ).first()
-
                 Schedule.objects.create(
                     func='apps.mappings.queue.construct_tasks_and_chain_import_fields_to_fyle',
                     args=workspace_id,
@@ -35,6 +32,10 @@ try:
                     minutes=24 * 60,
                     next_run=old_schedule.next_run
                 )
-
+        new_schedule = Schedule.objects.filter(
+            func='apps.mappings.queue.construct_tasks_and_chain_import_fields_to_fyle'
+        ).count()
+        print(f"Number of schedules created {new_schedule}")
+        raise Exception('Rollback')
 except Exception as e:
     print('Error:', e)
