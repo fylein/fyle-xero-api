38d9697d7386c3822fae27f56bcd24321b6ee16e
Hrishabh Tiwari <hrishabh.t@fyle.in>
2024-03-26 23:54:27 +0530

    Modified test cases for exports settings trigger

--- tests/test_workspaces/test_apis/test_export_settings/test_triggers.py
+++ tests/test_workspaces/test_apis/test_export_settings/test_triggers.py
@@ -1,11 +1,13 @@
+from datetime import datetime, timezone
 from apps.workspaces.apis.export_settings.triggers import ExportSettingsTrigger
-from apps.workspaces.models import WorkspaceGeneralSettings
+from apps.workspaces.models import WorkspaceGeneralSettings, LastExportDetail
 from apps.tasks.models import TaskLog, Error
 from apps.fyle.models import ExpenseGroup
 
 
 def test_post_save_workspace_general_settings_export_trigger(mocker, db, add_workspace_to_database):
-    workspace_id = 100
+    workspace_id = 1
+    LastExportDetail.objects.create(workspace_id=workspace_id, last_exported_at=datetime.now(timezone.utc))
     workspace_general_settings_instance, _ = WorkspaceGeneralSettings.objects.update_or_create(
         workspace_id=workspace_id,
         defaults={
@@ -14,17 +16,14 @@ def test_post_save_workspace_general_settings_export_trigger(mocker, db, add_wor
         }
     )
 
+    ExpenseGroup.objects.filter(workspace_id=workspace_id).exclude(
+        fund_source__in=['PERSONAL']
+    ).update(exported_at=None)
+
     expense_grp_ccc = ExpenseGroup.objects.filter(
         workspace_id=workspace_id,
         exported_at__isnull=True
     ).exclude(fund_source__in=['PERSONAL']).values_list('id', flat=True)
-
-    expense_grp_ccc_count = expense_grp_ccc.count()
-
-    before_delete_count = TaskLog.objects.filter(
-        workspace_id=workspace_id,
-        status__in=['FAILED', 'FATAL']
-    ).count()
 
     export_trigger = ExportSettingsTrigger()
     export_trigger.run_workspace_general_settings_triggers(
@@ -42,30 +41,28 @@ def test_post_save_workspace_general_settings_export_trigger(mocker, db, add_wor
     ).exclude(type__contains='_MAPPING').count()
 
     assert after_errors_count == 0
-    assert after_delete_count == before_delete_count - expense_grp_ccc_count
+    assert after_delete_count == 0
 
 
 def test_post_save_workspace_general_settings_export_trigger_2(mocker, db, add_workspace_to_database):
-    workspace_id = 100
+    workspace_id = 1
+    LastExportDetail.objects.create(workspace_id=workspace_id, last_exported_at=datetime.now(timezone.utc))
     workspace_general_settings_instance, _ = WorkspaceGeneralSettings.objects.update_or_create(
         workspace_id=workspace_id,
         defaults={
-            'reimbursable_expenses_object': 'JOURNAL ENTRY',
-            'corporate_credit_card_expenses_object': None
+            'reimbursable_expenses_object': None,
+            'corporate_credit_card_expenses_object': 'JOURNAL ENTRY'
         }
     )
+
+    ExpenseGroup.objects.filter(workspace_id=workspace_id).exclude(
+        fund_source__in=['CCC']
+    ).update(exported_at=None)
 
     expense_grp_personal = ExpenseGroup.objects.filter(
         workspace_id=workspace_id,
         exported_at__isnull=True
     ).exclude(fund_source__in=['CCC']).values_list('id', flat=True)
-
-    expense_grp_personal_count = expense_grp_personal.count()
-
-    before_delete_count = TaskLog.objects.filter(
-        workspace_id=workspace_id,
-        status__in=['FAILED', 'FATAL']
-    ).count()
 
     export_trigger = ExportSettingsTrigger()
     export_trigger.run_workspace_general_settings_triggers(
@@ -83,4 +80,4 @@ def test_post_save_workspace_general_settings_export_trigger_2(mocker, db, add_w
     ).exclude(type__contains='_MAPPING').count()
 
     assert after_errors_count == 0
-    assert after_delete_count == before_delete_count - expense_grp_personal_count
+    assert after_delete_count == 0
