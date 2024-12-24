df2517f1cd6795ca75eb396759f91a1047979cbe
ashwin1111 <winash1998@gmail.com>
2024-12-24 14:25:41 +0530

    fix lint

--- apps/fyle/migrations/0023_auto_20241224_0846.py
+++ apps/fyle/migrations/0023_auto_20241224_0846.py
@@ -17,9 +17,9 @@ class Migration(migrations.Migration):
                 DECLARE
                         tbl RECORD;
                 BEGIN
-                        FOR tbl IN 
-                        SELECT table_schema, table_name 
-                        FROM information_schema.tables 
+                        FOR tbl IN
+                        SELECT table_schema, table_name
+                        FROM information_schema.tables
                         WHERE table_type = 'BASE TABLE' AND table_schema = 'public'
                         LOOP
                         EXECUTE format('ALTER TABLE %I.%I REPLICA IDENTITY FULL', tbl.table_schema, tbl.table_name);
