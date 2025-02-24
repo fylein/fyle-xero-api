bc1bcc5e1266ebccdccdcea9b45abf9a7d4885a3
ashwin1111 <winash1998@gmail.com>
2025-02-24 21:42:01 +0530

    add retries

--- workers/export/worker.py
+++ workers/export/worker.py
@@ -20,6 +20,14 @@ class ExportWorker(EventConsumer):
     def process_message(self, routing_key, payload_dict, delivery_tag):
         try:
             logger.info('Processing message for workspace - %s with routing key - %s and payload - %s with delivery tag - %s', payload_dict['workspace_id'], routing_key, payload_dict, delivery_tag)
+
+            # We're gonna retry failed events since this queue is primarily webhook calls from Fyle, if this is a scheduled export, it doesn't necessarily needs to be retried
+            retry_count = payload_dict.get('retry_count', 0)
+            if retry_count >= 2:
+                logger.error('Message failed after %s retries, dropping message', retry_count)
+                self.qconnector.reject_message(delivery_tag, requeue=False)
+                return
+
             handle_exports(payload_dict['data'])
             self.qconnector.acknowledge_message(delivery_tag)
         except Exception as e:
@@ -27,14 +35,24 @@ class ExportWorker(EventConsumer):
 
     def handle_exception(self, routing_key, payload_dict, error, delivery_tag):
         logger.error('Error while handling exports for workspace - %s, error: %s', payload_dict, str(error))
+
+        payload_dict['retry_count'] = payload_dict.get('retry_count', 0) + 1
+
         FailedEvent.objects.create(
             routing_key=routing_key,
             payload=payload_dict,
             error_traceback=str(error),
             workspace_id=payload_dict['workspace_id'] if payload_dict.get('workspace_id') else None
         )
-        # Reject the message and do not requeue it
-        self.qconnector.reject_message(delivery_tag, requeue=False)
+
+        if payload_dict['retry_count'] < 2:
+            self.qconnector.reject_message(delivery_tag, requeue=True)
+        else:
+            self.qconnector.reject_message(delivery_tag, requeue=False)
+
+    def shutdown(self, _, __):
+        logger.info('Received signal %s, shutting down...', _)
+        super().shutdown()
 
 
 def consume():
