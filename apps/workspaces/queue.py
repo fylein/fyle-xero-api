139bf5eccff61e2d7b925f8b7fa47e2a039d13ae
ashwin1111 <winash1998@gmail.com>
2025-05-08 21:19:17 +0530

    feat: introduce real-time flag

--- apps/workspaces/queue.py
+++ apps/workspaces/queue.py
@@ -34,6 +34,7 @@ def schedule_sync(
     hours: int,
     email_added: List,
     emails_selected: List,
+    is_real_time_export_enabled: bool,
 ):
     ws_schedule, _ = WorkspaceSchedule.objects.get_or_create(workspace_id=workspace_id)
 
@@ -46,22 +47,23 @@ def schedule_sync(
         ws_schedule.start_datetime = datetime.now()
         ws_schedule.interval_hours = hours
         ws_schedule.emails_selected = emails_selected
+        ws_schedule.is_real_time_export_enabled = is_real_time_export_enabled
 
         for email in email_added:
             if email not in ws_schedule.additional_email_options:
                 ws_schedule.additional_email_options.append(email)
 
-        schedule, _ = Schedule.objects.update_or_create(
-            func="apps.workspaces.tasks.run_sync_schedule",
-            args="{}".format(workspace_id),
-            defaults={
+        if not is_real_time_export_enabled:
+            schedule, _ = Schedule.objects.update_or_create(
+                func="apps.workspaces.tasks.run_sync_schedule",
+                args="{}".format(workspace_id),
+                defaults={
                 "schedule_type": Schedule.MINUTES,
                 "minutes": hours * 60,
                 "next_run": datetime.now() + timedelta(hours=hours),
-            }
-        )
-
-        ws_schedule.schedule = schedule
+                }
+            )
+            ws_schedule.schedule = schedule
 
         ws_schedule.save()
 
