b964f87b597457e66412bf1ccd3da5aebb115187
Hrishabh Tiwari <74908943+Hrishabh17@users.noreply.github.com>
2025-05-27 13:48:29 +0530

    fix: remove dead code for supplier import (#506)
    * fix: remove dead code for supplier import
    
    * bump rest auth version

--- apps/xero/actions.py
+++ apps/xero/actions.py
@@ -1,13 +1,9 @@
 from datetime import datetime, timezone
 
-from django_q.tasks import Chain
-from fyle_accounting_mappings.models import MappingSetting
+from apps.mappings.queue import construct_tasks_and_chain_import_fields_to_fyle
 
-from apps.fyle.enums import FyleAttributeEnum
 from apps.workspaces.models import Workspace, WorkspaceGeneralSettings, XeroCredentials
 from apps.xero.utils import XeroConnector
-from apps.mappings.helpers import is_auto_sync_allowed
-from apps.mappings.constants import SYNC_METHODS
 
 
 def get_xero_connector(workspace_id):
@@ -39,53 +35,12 @@ def sync_dimensions(workspace_id):
 
 def refersh_xero_dimension(workspace_id):
     workspace_id = workspace_id
-    xero_credentials = XeroCredentials.get_active_xero_credentials(
-        workspace_id=workspace_id
-    )
     xero_connector = get_xero_connector(workspace_id=workspace_id)
 
-    mapping_settings = MappingSetting.objects.filter(
-        workspace_id=workspace_id, import_to_fyle=True
-    )
     workspace_general_settings: WorkspaceGeneralSettings = WorkspaceGeneralSettings.objects.filter(workspace_id=workspace_id).first()
 
-    ALLOWED_SOURCE_FIELDS = [
-        FyleAttributeEnum.PROJECT,
-        FyleAttributeEnum.COST_CENTER,
-    ]
-
     if workspace_general_settings:
-        chain = Chain()
-        for mapping_setting in mapping_settings:
-            if mapping_setting.source_field in ALLOWED_SOURCE_FIELDS or mapping_setting.is_custom:
-                # run new_schedule_or_delete_fyle_import_tasks
-                chain.append(
-                    'fyle_integrations_imports.tasks.trigger_import_via_schedule',
-                    workspace_id,
-                    mapping_setting.destination_field,
-                    mapping_setting.source_field,
-                    'apps.xero.utils.XeroConnector',
-                    xero_credentials,
-                    [SYNC_METHODS.get(mapping_setting.destination_field.upper(), 'tracking_categories')],
-                    is_auto_sync_allowed(workspace_general_settings, mapping_setting),
-                    False,
-                    None,
-                    mapping_setting.is_custom,
-                    q_options={
-                        'cluster': 'import'
-                    }
-                )
-            elif workspace_general_settings.import_suppliers_as_merchants:
-                # run auto_create_suppliers_as_merchant
-                chain.append(
-                    "apps.mappings.tasks.auto_create_suppliers_as_merchants", workspace_id,
-                    q_options={
-                        'cluster': 'import'
-                    }
-                )
-
-        if chain.length() > 0:
-            chain.run()
+        construct_tasks_and_chain_import_fields_to_fyle(workspace_id)
 
     xero_connector.sync_dimensions(workspace_id)
 
