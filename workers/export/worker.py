a4975b4523b2c26cffab3cc3474b6a9bcf000333
Rushikesh Tayade <rushikesh.tayade@sage.com>
2025-12-11 13:16:33 +0530

    fix: create cache table from export worker (#595)
    * fix: create cache table from export worker
    
    * Fix comment

--- workers/export/worker.py
+++ workers/export/worker.py
@@ -1,16 +1,17 @@
+import logging
 import os
 import signal
-import logging
 import traceback
 
-from .actions import handle_exports
+from workers.export.actions import handle_exports
 
-from fyle_accounting_library.fyle_platform.enums import RoutingKeyEnum
-from fyle_accounting_library.rabbitmq.models import FailedEvent
-from fyle_accounting_library.rabbitmq.data_class import RabbitMQData
-from consumer.event_consumer import EventConsumer
-from common.qconnector import RabbitMQConnector
 from common.event import BaseEvent
+from common.qconnector import RabbitMQConnector
+from consumer.event_consumer import EventConsumer
+from fyle_accounting_library.fyle_platform.enums import RoutingKeyEnum
+from fyle_accounting_library.rabbitmq.data_class import RabbitMQData
+from fyle_accounting_library.rabbitmq.helpers import create_cache_table
+from fyle_accounting_library.rabbitmq.models import FailedEvent
 
 
 logger = logging.getLogger('workers')
@@ -65,6 +66,7 @@ class ExportWorker(EventConsumer):
 
 
 def consume():
+    create_cache_table()
     rabbitmq_url = os.environ.get('RABBITMQ_URL')
 
     export_worker = ExportWorker(
